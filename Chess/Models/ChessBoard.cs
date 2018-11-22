360c97a378019f348072c856eed51d38ad5b71c6
Rahul Vats <vats.rahul@gmail.com>
2018-11-22 11:41:23 -0600

    Partial Classes Created

---
+++ Chess/Models/ChessBoard.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess.Models
+{
+    public static class ChessBoard
+    {
+        private const int Rows = 8;
+        private const int Columns = 8;
+    }
+    public static class ChessBoardCoordinates
+    {
+        // protected Piece[][] board;
+        // protected List<Move> moveList;
+    }
+}
