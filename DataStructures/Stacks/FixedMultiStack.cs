1588a823ec8cb727d54ae61b450e4ad30960f295
Rahul Vats <vats.rahul@gmail.com>
2018-11-24 15:45:10 -0600

    Fixed Capacity Multiple Stacks Using Array

---
+++ DataStructures/Stacks/FixedMultiStack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Stacks
+{
+    class FixedMultiStack
+    {
+        private int NumberOfStacks = 3;
+        private int StackCapacity;
+        private int[] Values;
+        private int[] Sizes;
+
+        public FixedMultiStack(int stackSize)
+        {
+            StackCapacity = stackSize;
+            Values = new int[stackSize * NumberOfStacks];
+            Sizes = new int[NumberOfStacks];
+        }
+
+        public void Push(int stackNum, int value)
+        {
+            if (IsFull(stackNum))
+            {
+                throw new StackOverflowException();
+            }
+            Sizes[stackNum]++;
+            Values[IndexOfTop(stackNum)] = value;
+        }
+
+        public int Pop(int stackNum)
+        {
+            if (IsEmpty(stackNum))
+            {
+                throw new StackOverflowException();
+            }
+            int topIndex = IndexOfTop(stackNum);
+            int value = Values[IndexOfTop(stackNum)];
+            Values[topIndex] = 0;
+            Sizes[stackNum]--;
+            return value;
+        }
+
+        public int Peek(int stackNum)
+        {
+            if (IsEmpty(stackNum))
+            {
+                throw new StackOverflowException();
+            }
+            return Values[IndexOfTop(stackNum)];
+        }
+
+        public bool IsFull(int stackNum)
+        {
+            return Sizes[stackNum]==StackCapacity;
+        }
+
+        public bool IsEmpty(int stackNum)
+        {
+            return Sizes[stackNum] == 0;
+        }
+
+        private int IndexOfTop(int stackNum)
+        {
+            int offSet = stackNum*StackCapacity;
+            int size = Sizes[stackNum];
+            return offSet + size - 1;
+        }
+    }
+}
